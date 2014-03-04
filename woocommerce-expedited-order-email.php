ac46628dbdd547998c92eb599665920a1161207a
Max Rice <max@maxrice.com>
2014-03-04 15:37:40 -0500

    require_once

--- woocommerce-expedited-order-email.php
+++ woocommerce-expedited-order-email.php
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 function add_expedited_order_woocommerce_email( $email_classes ) {
 
 	// include our custom email class
-	require( 'includes/class-wc-expedited-order-email.php' );
+	require_once( 'includes/class-wc-expedited-order-email.php' );
 
 	// add the email class to the list of email classes that WooCommerce loads
 	$email_classes['WC_Expedited_Order_Email'] = new WC_Expedited_Order_Email();
